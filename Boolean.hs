769f4a9e9353ab6bb2f80ae4cce2aeddf286b418
JJoaoll <joaoduos@gmail.com>
2024-10-05 12:05:10 -0300

    Booleans

---
+++ Boolean.hs
@@ -0,0 +1,17 @@
+import Data.Bits (Bits(xor))
+data Boolean = T | F deriving(Show, Eq) 
+lnot :: Boolean -> Boolean 
+lnot T = F
+lnot _ = T 
+
+land :: Boolean -> Boolean -> Boolean 
+land T T = T
+land _ _ = F
+
+lor :: Boolean -> Boolean -> Boolean 
+lor T _ = T 
+lor _ x = x 
+
+ifthenelse :: Boolean -> a -> a -> a 
+ifthenelse T x _ = x 
+ifthenelse F _ y = y
